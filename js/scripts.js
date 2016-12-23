119dd1f4e92aaeea1302eb859200793c3e11e67c
Nathan Wentworth <nwentworth95@gmail.com>
2016-12-22 19:53:38 -0500

    cool

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+
+function Load() {
+  var links = localStorage.getItem('links');
+  var textarea = document.getElementById('textarea');
+  textarea.value = links;
+
+  ParseLinks(links);
+  
+}
+Load();
+
+function ParseLinks(links) {
+  var linksArr;
+  if (links != null) {
+    linksArr = links.split('\n');
+  }
+  console.log(linksArr);
+  console.log(links);
+  CreateLinks(linksArr);
+}
+
+function CreateLinks(linksArr) {
+  
+  var list = document.getElementById('list');
+
+  if (linksArr != null && linksArr[0] != "") {
+    for (var i = 0; i < linksArr.length; i++) {
+
+      var li = document.createElement('li');
+      var a = document.createElement('a');
+      a.setAttribute('href', 'http://' + linksArr[i].split(' ')[0])
+      var linkText = linksArr[i].slice(linksArr[i].indexOf(' '));
+      a.textContent = linkText;
+      li.appendChild(a);
+      list.appendChild(li);
+    }
+  } else {
+    var li = document.createElement('li');
+    li.innerHTML = "Click \"edit\" to add links!<br>Use the format: example.com example<br>URL, a space, and then the title.";
+    list.appendChild(li);
+  }
+}
+  
+function EditLinks() {
+  console.log("edit links clicked");
+  var editSection = document.getElementById('edit');
+  if (editSection.style.display != "none") {
+    editSection.style.display = "none";
+  } else {
+    editSection.style.display = "block";
+  }
+
+  var textarea = document.getElementById('textarea');
+  var links = textarea.value;
+
+  localStorage.setItem('links', links);
+  var elements = document.getElementsByTagName('li');
+  while (elements[0]) elements[0].parentNode.removeChild(elements[0]);
+  Load();
+}
+
+function time() {
+  var d = new Date();
+  var hr = d.getHours();
+  var min = d.getMinutes();
+
+  if (hr > 12) {
+    hr = hr - 12;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }  
+  
+  document.getElementById("hour").textContent = "" + hr;
+  document.getElementById("minute").textContent = "" + min;
+}
+time();
+setInterval(time, 500);
+
